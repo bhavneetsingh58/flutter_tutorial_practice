7c932f836d5e9d11637ab81ed05b1b42d9ff35b7
bhavneetsingh58 <46424237+bhavneetsingh58@users.noreply.github.com>
2020-06-11 05:47:20 +0530

    day3

---
+++ lib/sliderDrawer.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class sliderDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+      child:ListView(
+        padding: const EdgeInsets.all(0),
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text('Bhavneet'),
+            accountEmail: Text('Developer'),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage:AssetImage("asset/images/Bhavneet_Lightweight.jpeg"), )
+            
+          ),
+          
+          ListTile
+          (leading: Icon(Icons.person),
+          title: Text("Bhavneet Singh"),
+          subtitle: Text("Developer"),
+          onTap:(){},
+          ),
+          ListTile
+          (leading: Icon(Icons.person),
+          title: Text("Email"),
+          subtitle: Text("bhavneetsachdev58@gmail.com"),
+          onTap:(){},
+          ),
+          ListTile
+          (leading: Icon(Icons.person),
+          title: Text("Contact Info"),
+          subtitle: Text("8080008663"),
+          onTap:(){},
+          ),
+        ],
+      ),
+    
+      ),
+    );
+  }
+}
