61e383b4a2eab30e9696eb2dff84dd0c1d2a50c9
bhavneetsingh58 <46424237+bhavneetsingh58@users.noreply.github.com>
2020-06-11 07:15:11 +0530

    Stateleful

--- lib/main.dart
+++ lib/main.dart
@@ -21,26 +21,51 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var myText ="Change My Name";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Welcome to My World!"),),
 
 
-    body: Center(child: Card(
-      child: Column(
-        children: <Widget>[
-      Image.asset("assets/images/Triple_monitor.jpg"),
-    ],
-    ),
+    body: Center(child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SingleChildScrollView(
+              child: Card(
+          child: Column(
+            children: <Widget>[
+          Image.asset("assets/images/developer_phone.jpg",width:370),
+          SizedBox(height: 20,),
+          Text(myText,
+          style: TextStyle(fontWeight: FontWeight.bold,fontSize:25),
+          ),
+          SizedBox(height: 10,),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: "Enter Some Text",
+            labelText: "Name",),
+            ),
+          ),
+
+        ],
+        ),
+        ),
+      ),
     ),
     ),
     
     
    
     
-    floatingActionButton: FloatingActionButton(onPressed: (){},child: Icon(Icons.phone),),  
+    floatingActionButton: FloatingActionButton(onPressed: (){},child: Icon(Icons.send),),  
     //SliderDrawer.dart
     );
   }
